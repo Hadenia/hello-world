57a8396f714e6e9acda3e526dbaf32a64d2beca8
Hadenia <42811357+Hadenia@users.noreply.github.com>
2018-09-08 12:49:29 -0300

    Create Questao12.c

---
+++ Questao12.c
@@ -0,0 +1,20 @@
+//UFRN - Programação Avançada
+//BC&T - Ênfase em Tecnologia de Computação
+//Hadênia Rodrigues Ferreira
+//Questão 12
+
+int main(){
+    float aloha[10], coisas[10][5], *pf, value = 2.2;
+    int i=3;
+
+     aloha[2] = value;
+     scanf("%f", &aloha);
+    // aloha = value;  //Invalido
+     printf("%f", aloha);
+     coisas[4][4] = aloha[3];
+    // coisas[5] = aloha; //Invalido
+    // pf = value; //Invalido
+     pf = aloha;
+     //Demais comandos são válidos
+
+}
