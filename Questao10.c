0924170b15855a4d0bfa2426b244d51894b2c217
Hadenia <42811357+Hadenia@users.noreply.github.com>
2018-09-08 12:16:26 -0300

    Create Questao10.c

---
+++ Questao10.c
@@ -0,0 +1,32 @@
+//UFRN - Programação Avançada
+//BC&T - Ênfase em Tecnologia de Computação
+//Hadênia Rodrigues Ferreira
+//Questão 10
+
+
+/* x for declarado como char? 1
+char x1[4] == 4092;
+x1+1 == 4093;
+x1+2 == 4094;
+x1+3 == 4095;
+
+x for declarado como int? 2
+int x2[4] == 4092;
+x2+1 == 4094;
+x2+2 == 4096;
+x2+3 == 4098;
+
+x for declarado como float? 4
+float x3[4] == 4092;
+x3+1 == 4096;
+x3+2 == 4100;
+x3+3 == 4104;
+
+
+x for declarado como double? 8
+double x4[4] == 4092;
+x4+1 == 4100;
+x4+2 == 4108;
+x4+3 == 4116;
+
+*/
