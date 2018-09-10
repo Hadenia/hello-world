0d01641bfb8ce6b2d579e9a7d40b1b3060d3abd4
Hadenia <42811357+Hadenia@users.noreply.github.com>
2018-09-10 18:40:19 -0300

    Update Questao16.c

--- Questao16.c
+++ Questao16.c
@@ -12,21 +12,35 @@ int compare (const void * a, const void * b)
   return ( *(float*)a - *(float*)b );
 }
 
+int i, j;
+void ordem(float *x, int n){
+    float y=0;
+    for (i=0; i<n; i++){
+      for (j=i+1; j<n; j++){
+        if(x[i]>x[j]){
+            y = x[i];
+            x[i] = x[j];
+            x[j]= y;
+        }
+     }
+    }
+}
+
 int main(int argc)
 {
-    int i;
     int n = 5;
     float *x;
+    int r;
 
     //Aloca memoria para x
     x = (float*) malloc(n*sizeof(float));
 
     //declaração dos valores de x
-    x[0] = 7.9;
-    x[1] = 6.4;
-    x[2] = 9.3;
-    x[3] = 3.9;
-    x[4] = 1.5;
+    x[0] = 1.9;
+    x[1] = 0.4;
+    x[2] = 3.3;
+    x[3] = 2.9;
+    x[4] = 9.5;
 
     //Apresenta a matriz original
     printf("Matriz original \n");
@@ -36,7 +50,15 @@ int main(int argc)
     printf("\n \n");
 
     void (*pt)(void *x, size_t n, size_t size, int(*compare)(const void *, const void *));
-    pt = qsort;
+
+    printf("Digite 1 para qsort ou digite 2 para bubble sort");
+    scanf("%d", &r);
+
+    if(r == 1){
+        pt = qsort;
+    }else if(r ==2){
+        pt = ordem;
+    }
 
     (*pt)(x, n, sizeof(float), compare);
 
